6d7417844e45c64ce4d42d8cc2d4d7fab18813b3
Kim Scicluna <k.scicluna1337@gmail.com>
2021-10-31 19:26:15 +0100

    Added small chance to wake up the whole building. Check for collision with zombies - will aggro zombies Check for player sneaking - if not sneaking inside, will aggro zombies Refactoring and improved performance

--- Contents/mods/FearTheSun/media/lua/client/FearTheSun.lua
+++ Contents/mods/FearTheSun/media/lua/client/FearTheSun.lua
@@ -2,6 +2,7 @@
 
 building_table = {};
 
+local pillowmod;
 
 --as squares are loaded, add the associated buildings to a table
 function addBuildingList(_square)
@@ -35,20 +36,42 @@ end
 
 --calc the IsDay variable
 function calculateHour()
-    pillowmod = getPlayer():getModData();
     pillowmod.currentHour = math.floor(math.floor(GameTime:getInstance():getTimeOfDay() * 3600) / 3600);
+
     if pillowmod.currentHour >= 5 and pillowmod.currentHour <= 19 then
         pillowmod.IsDay = true
-    else pillowmod.IsDay = false end 
-
+    else 
+        pillowmod.IsDay = false 
+    end 
 end --end calc hour function 
 
-function updateZCounter()
+--calc the IsRaining variable
+function calculateRain()
+    if RainManager.isRaining() then
+        pillowmod.IsRaining = false;
+    end;
+end
+
+function initialise()
     pillowmod = getPlayer():getModData();
-    if pillowmod.zCounter == nil or pillowmod.zCounter >= 2000
-        then pillowmod.zCounter = 1;
-        elseif pillowmod.zCounter <= 2001
-        then pillowmod.zCounter = pillowmod.zCounter + 1;
+    calculateHour();
+    calculateRain();
+end
+
+function isDay()
+    return pillowmod.IsDay ~= nil and pillowmod.IsDay;
+end
+
+function isRaining()
+    return pillowmod.IsRaining ~= nil and pillowmod.IsRaining;
+end
+
+function updateZCounter()
+    local playerData = getPlayer():getModData();
+    if playerData.zCounter == nil or playerData.zCounter >= 2000
+        then playerData.zCounter = 1;
+        elseif playerData.zCounter <= 2001
+        then playerData.zCounter = playerData.zCounter + 1;
     else end 
 end 
 
@@ -97,139 +120,194 @@ function zombieStop(zombie)
 end
 
 function isZombieIdle(zombie)
-    if zombie:isMoving() == false
-        then b = true;
-        else b = false;
+    if zombie ~= nil and zombie:isMoving() == false then 
+        return true;
+    else 
+        return false;
     end 
-    return b
 end
 
-function isZombieOutside(zombie)
-    if zombie:getCurrentSquare():isOutside() == false 
-        then b = false;
-        else b = true;
+function isSquareOutside(square)
+    if square:isOutside() == false then 
+        return false;
+    else 
+        return true;
     end
-    return b 
 end
 
-function zResetCommand(zombie)
-    zombie:getModData().commandSent = false;
+function isCharacterOutside(character)
+    local currentSquare = character:getCurrentSquare();
+    return isSquareOutside(currentSquare);
+end
+
+function zResetCommand(zombieModData)
+    zombieModData.commandSent = false;
 end 
 
 function zombieHasCommand(zombie)
-    if zombie:getModData().commandSent == nil 
-    then 
-        b = false;
+    local zombieModData = zombie:getModData();
+
+    if zombieModData.commandSent == nil then 
+        return false;
     else 
-        b =  zombie:getModData().commandSent;
+        return zombieModData.commandSent;
     end
-    return b
 end
 
 function zCheck(zombie)
+    
+    local zombieModData = zombie:getModData();
 
     --initialize wake tick, decrement it
-    if zombie:getModData().awakeTick == nil or zombie:getModData().awakeTick < 0
-        then zombie:getModData().awakeTick = 1;
-    else zombie:getModData().awakeTick = zombie:getModData().awakeTick - 1;
+    if zombieModData.awakeTick == nil or zombieModData.awakeTick < 0 then 
+        zombieModData.awakeTick = 1;
+    else 
+        zombieModData.awakeTick = zombieModData.awakeTick - 1;
     end 
 
-    if isZombieOutside(zombie) == false and zombie:getModData().awakeTick > 1 
-        then return
-    else end 
-
-    if pillowmod.IsDay 
-        then
-            zDayRoutine(zombie);
-        else
-            zNightRoutine(zombie);
+    if isCharacterOutside(zombie) == false and zombieModData.awakeTick > 1 then 
+        return;
     end
-
+    
+    if isDay() and not isRaining() then
+        zDayRoutine(zombie, zombieModData);
+    else
+        zNightRoutine(zombie, zombieModData);
+    end
 end
 
-function zDayRoutine(zombie)
+function zDayRoutine(zombie, zombieModData)
     -- day, inside
-    if zombieHasCommand(zombie) == false and isZombieOutside(zombie) == false and pillowmod.zCounter <= 1000
+    if zombieHasCommand(zombie) == false and isCharacterOutside(zombie) == false and pillowmod.zCounter <= 1000
         then 
-            zombie:getModData().commandSent = true;
+            zombieModData.commandSent = true;
             zombie:setUseless(true); 
-            zombie:getModData().docile = true;
+            zombieModData.docile = true;
             zombie:DoZombieStats();
     -- day, outside, lure via sound or location
-    elseif (zombieHasCommand(zombie) == false and isZombieOutside(zombie) and pillowmod.zCounter >= 300 and pillowmod.zCounter <=1099 ) 
-        or(zombieHasCommand(zombie) == false and isZombieOutside(zombie) and isZombieIdle(zombie))
+    elseif (zombieHasCommand(zombie) == false and isCharacterOutside(zombie) and pillowmod.zCounter >= 300 and pillowmod.zCounter <=1099 ) 
+        or(zombieHasCommand(zombie) == false and isCharacterOutside(zombie) and isZombieIdle(zombie))
         then 
-            zombie:getModData().commandSent = true;
-            zombie:getModData().docile = false;
-            if ZombRand(1)==0
-                then lureZombiePathSound(zombie);
-                else lureZombiePathLocation(zombie);
+            zombieModData.commandSent = true;
+            zombieModData.docile = false;
+            if ZombRand(1)==0 then 
+                lureZombiePathSound(zombie);
+            else 
+                lureZombiePathLocation(zombie);
             end 
     -- day, help un-stuck zombie
-    elseif zombieHasCommand(zombie) == false and isZombieOutside(zombie) and pillowmod.zCounter >= 1799 and pillowmod.zCounter <= 1899
+    elseif zombieHasCommand(zombie) == false and isCharacterOutside(zombie) and pillowmod.zCounter >= 1799 and pillowmod.zCounter <= 1899
         then 
-            zombie:getModData().commandSent = true;
-            zombie:getModData().docile = false;
+            zombieModData.commandSent = true;
+            zombieModData.docile = false;
             zombieStop(zombie);
     elseif pillowmod.zCounter >= 1900
-        then zResetCommand(zombie) ;
+        then zResetCommand(zombieModData) ;
     else end
-
 end 
 
-function zNightRoutine(zombie)
-
+function zNightRoutine(zombie, zombieModData)
         if pillowmod.zCounter  == 100
             then 
             zombie:setMoving(true);
             zombie:setVariable("bMoving", true);
             zombie:setFakeDead(false);
             zombie:setUseless(false); 
-            zombie:getModData().docile = false;
+            zombieModData.docile = false;
             zombie:DoZombieStats();
-            zResetCommand(zombie) ;
+            zResetCommand(zombieModData) ;
         else end
 end
 
+function aggroZombie(zombie)
+    local zombieModData = zombie:getModData();
+    zombie:setFakeDead(false);
+    zombie:setUseless(false); 
+    zombieModData.docile = false;
+    zombieModData.awakeTick = 500;
+end
 
-
-
---not sure any other methods to get the zombie list, so get the whole cell
---compare each one to the same room as player, and then wake them up.
-function smackZombie()
-    currentroom = getPlayer():getCurrentSquare():getRoom();
-    print("smacked a zombie, waking up it's friends.");
-    local zlist = getPlayer():getCell():getZombieList();
-            if(zlist ~= nil) then
-                for i=0, zlist:size()-1 do
-                    if  zlist:get(i):getCurrentSquare():getRoom() == currentroom
-                        then 
-                            zlist:get(i):setFakeDead(false);
-                            zlist:get(i):setUseless(false); 
-                            zlist:get(i):getModData().docile = false;
-                            zlist:get(i):getModData().awakeTick = 500;
-                    else end
-                end
+function wakeUpZombiesInTargetsRoom(target)
+    if target ~= nil then
+        local currentRoom = target:getCurrentSquare():getRoom();
+        print("Waking up zombies in room.");
+        local zlist = player:getCell():getZombieList();
+        if(zlist ~= nil) then
+            for i=0, zlist:size()-1 do
+                local zombie = zlist:get(i);
+                if zombie:getCurrentSquare():getRoom() == currentRoom then 
+                    aggroZombie(zombie);
+                else end
+            end
         end
-end 
+    end
+end
 
-function zIgniteZombie(zombie)
-    end 
+function wakeUpZombiesInTargetsBuilding(target)
+    if target ~= nil then
+        local currentBuilding = target:getCurrentSquare():getBuilding();
+        print("Unlucky! Waking up zombies in building.");
+        local zlist = player:getCell():getZombieList();
+        if(zlist ~= nil) then
+            for i=0, zlist:size()-1 do
+                local zombie = zlist:get(i);
+                if zombie:getCurrentSquare():getBuilding() == currentBuilding then 
+                    aggroZombie(zombie);
+                else end
+            end
+        end
+    end
+end
 
+--Each triggering action has a small chance to wake up the whole building
+function rollSetZombieActive(target)
+    local random = ZombRand(1, 100);
+    if random < 95 then --95% chance of waking zombies in room
+        wakeUpZombiesInTargetsRoom(target);
+    else --5% chance of waking zombies in building
+        wakeUpZombiesInTargetsBuilding(target);
+    end
+end
 
+function onHitOrCollideWithZombie(source, target)
+    --xor, check if collision/hit is between zombie and player
+    local onlyOneIsPlayer = source ~= nil and target ~= nil and source:isZombie() ~= target:isZombie();
 
+    if onlyOneIsPlayer then
+        print("Hit or collided with zombie");
+        if isCharacterOutside(source) == false then
+            rollSetZombieActive(source)
+        elseif  isCharacterOutside(target) == false then
+            rollSetZombieActive(target)
+        end
+    end
+end
+
+function onMoveWhileInside()
+    local player = getPlayer();
+    if player:isSneaking() == false and isCharacterOutside(player) == false then
+        print("Did not sneak while inside");
+        rollSetZombieActive(player);
+    end
+end
+
+Events.OnGameStart.Add(initialise);
 
 --calc hour must be done at game start and then every hour because it initializes the time of day variable
-Events.OnGameStart.Add(calculateHour);
 Events.EveryHours.Add(calculateHour);
+
+--calc rain done every ten minutes to check for rain updates
+Events.EveryTenMinutes.Add(calculateRain);
 
 --gridsquare functions that were modified from original fear the rain.
 Events.LoadGridsquare.Add(addBuildingList);
 Events.ReuseGridsquare.Add(removeBuildingList);
 
+--Check for each zombie
 Events.OnZombieUpdate.Add(zCheck);
 Events.OnPlayerUpdate.Add(updateZCounter);
 
-
 --waking zombie functions, every hour since it really should happen just once unlesss player moves to a new cell
-Events.OnWeaponHitCharacter.Add(smackZombie);
+Events.OnWeaponHitCharacter.Add(onHitOrCollideWithZombie);
+Events.OnCharacterCollide.Add(onHitOrCollideWithZombie);
+Events.OnPlayerMove.Add(onMoveWhileInside);
